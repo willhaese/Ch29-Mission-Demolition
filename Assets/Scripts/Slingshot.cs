866b6e906e678e3f97ddfe957b15fce8cf34c5b8
William Haesemeyer <willhaese@gmail.com>
2017-06-14 20:11:27 -0400

    Halos Work
    Got halo to show on mouse over

---
+++ Assets/Scripts/Slingshot.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+public class Slingshot : MonoBehaviour
+{
+    // fields set in the Unity Inspector Pane
+    public GameObject prefabProjectile;
+    public bool ___________________________;
+    //fields set dynamically
+    public GameObject launchPoint;
+    void Awake()
+    {
+        Transform launchPointTrans = transform.Find("LaunchPoint");
+        launchPoint = launchPointTrans.gameObject;
+        launchPoint.SetActive(false);
+    }
+
+    void OnMouseEnter()
+    {
+        //print("Slingshot:OnMouseEnter()");
+        launchPoint.SetActive(true);
+
+    }
+    void OnMouseExit()
+    {
+        //print("Slingshot:OnMouseExit()");
+        launchPoint.SetActive(false);
+    }
+}

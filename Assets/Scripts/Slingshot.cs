af0222214cc567075dad11702885e2e9e2f55613
William Haesemeyer <willhaese@gmail.com>
2017-06-14 21:57:55 -0400

    Path Tracer Added
    A Path is now shown with a line that follows our projectile

--- Assets/Scripts/Slingshot.cs
+++ Assets/Scripts/Slingshot.cs
@@ -2,6 +2,7 @@
 using System.Collections;
 public class Slingshot : MonoBehaviour
 {
+    static public Slingshot S;
     // fields set in the Unity Inspector Pane
     public GameObject prefabProjectile;
     public float velocityMult = 4f;
@@ -18,6 +19,8 @@ public class Slingshot : MonoBehaviour
 
     void Awake()
     {
+        //Set the Slingshot singleton S
+        S = this;
         Transform launchPointTrans = transform.Find("LaunchPoint");
         launchPoint = launchPointTrans.gameObject;
         launchPoint.SetActive(false);
